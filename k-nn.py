f616afed1a2df24b1dd10cded9709a0b2cdad0a6
Gabe <ballcrushingenthusiast@protonmail.com>
2022-12-31 15:28:36 +0000

    Update k-nn.py

--- k-nn.py
+++ k-nn.py
@@ -17,18 +17,14 @@ def load_data_from_csv(input_csv):
         labels.append(label[0])
     return feature_names, instances, labels
 
-
 input_test_csv = 'reviews_Video_Games_test.csv'
 input_training_csv = 'reviews_Video_Games_training.csv'
 
 training_feature_names, training_instances, training_labels = load_data_from_csv(input_csv = input_training_csv)
 test_feature_names, test_instances, test_labels = load_data_from_csv(input_csv = input_test_csv)
 
-
 classifier = KNeighborsClassifier(n_neighbors = 2)
 classifier.fit(training_instances, training_labels)
 predicted_test_labels = classifier.predict(test_instances)
 
 print(classification_report(test_labels, predicted_test_labels, digits = 3))
-
-
