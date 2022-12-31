84845257bacb401c638b63b41cbb07be3ab81b8a
Gabe <ballcrushingenthusiast@protonmail.com>
2022-12-31 15:28:17 +0000

    Update decision tree.py

--- decision tree.py
+++ decision tree.py
@@ -8,7 +8,6 @@ from sklearn.metrics import classification_report
 input_test_csv = pd.read_csv('reviews_Video_Games_test.csv')
 input_training_csv = pd.read_csv('reviews_Video_Games_training.csv')
 
-
 train_x = input_training_csv.drop('data', axis=1)
 train_y = input_training_csv['data']
 test_x = input_test_csv.drop('data', axis=1)
@@ -16,8 +15,6 @@ test_y = input_test_csv['data']
 
 dtree = DecisionTreeClassifier(max_depth=2)
 dtree.fit(test_x, test_y)
-
-
 
 predictions = dtree.predict(test_x)
 acc = accuracy_score(test_y, predictions)
